0177013d268ddec48fd285d59f7663388ff7ac86
Cristian perez <kristiancrysis@gmail.com>
2018-04-10 21:40:15 -0300

    Se crea clase "ClienteCollection" Se agrega variable de retorno y la que  necesitan los metodos. Se agrega comentarios con las reglas de negocios para cada metodo.

--- ABP_BeLife/Belife/Cliente.cs
+++ ABP_BeLife/Belife/Cliente.cs
@@ -34,33 +34,77 @@ namespace Belife
             this.EstadoCivil = new EstadoCivil();
         }
 
-        public void Create()
+        //se utiliza con registro cliente
+        //todos los datos son obligatorios
+        public Boolean Create(Cliente cliente)
         {
+            Boolean varRetorno = false;
 
+            /*
+             * El cliente debe ser mayor de 18, esto se debe verificar en la interfaz.
+             * se envian los datos del cliente resivido a la bd
+             * en un try catch enviar información de retorno si datos son incorrectos
+             */
+
+            return varRetorno;
         }
-        public void Read()
-        {
 
+
+
+
+        //se utiliza conregistro cliente
+        public Boolean Update(Cliente cliente)
+        {
+            Boolean varRetorno = false;
+            /*
+             * verifica si existe el rut del cliente en la base de datos
+             * El cliente debe ser mayor de 18, esto se debe verificar en la interfaz.
+             * update resto de datos del cliente, si no existe notificar
+             */
+            return varRetorno;
         }
-        public void Update()
+        //se utiliza con registro cliente
+        public Boolean Delete(string rut)
         {
-
+            Boolean varRetorno = false;
+            //verificar si rut existe en bd
+            // no se puede eliminar un cliente que tenga contratos asociados, vigentes o no.
+            return varRetorno;
         }
-        public void Delete()
-        {
 
+        //se utiliza con listado clientes, registro cliente y ListadoContratos
+        public ClienteCollection Read(string rut)
+        {
+            ClienteCollection clientes = new ClienteCollection();
+            /*
+             *se verifica si el cliente existe en la bd.
+             * si existe se cargan los datos.
+             * de lo contrario se notifica (try catch).
+             */
+            return clientes;
         }
-        public void LeerTodo()
+        //se utiliza con listado clientes
+        public ClienteCollection LeerTodo()
         {
-
+            ClienteCollection clientes = new ClienteCollection();
+            //al presionar el boton "ListarTodos" se debe ejecutar este metodo
+            return clientes;
         }
-        public void LeerPorSexo()
+        //se utiliza con listado clientes
+        public ClienteCollection LeerPorSexo(Sexo sexo)
         {
-
+            ClienteCollection clientes = new ClienteCollection();
+            //al presionar el boton Buscar se ejecutara este metodo (si es que se a seleccionado un sexo)
+            //verificar si se a seleccionado un estado civil para ejecutar el metodo "LeerPorEstadoCivil" aqui.
+            return clientes;
         }
-        public void LeerPorEstadoCivil()
+        //se utiliza con listado clientes
+        public ClienteCollection LeerPorEstadoCivil(EstadoCivil estadoCivil)
         {
-
+            ClienteCollection clientes = new ClienteCollection();
+            //al presionar el boton Buscar se ejecutara este metodo (si es que se a seleccionado un estadoCivil)
+            //verificar si se a seleccionado un sexo para ejecutar el metodo "LeerPorSexo" aqui.
+            return clientes;
         }
 
     }
